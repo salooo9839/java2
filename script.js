cc9d9d7c459712fad0b0661a3143f33da5402794
SK CYBER <skyjan128@gmail.com>
2026-01-28 18:14:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+// String
+let studentName = "salman";
+let city = "lakki";
+
+// Number
+let age = 18;
+let price = 100;
+
+// Boolean
+let isStudent = true;
+
+// Undefined (declared but no value)
+let grade;
+
+// Null (empty value)
+let result = null;
+
+
+
+console.log("Type of studentName:", typeof studentName);
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
+console.log("Type of grade:", typeof grade);
+console.log("Type of result:", typeof result); // object (this is normal in JavaScript)
+
+
+
+// convert
+
+// Convert string to number
+let stringToNumber = Number("25");
+
+// Convert number to string
+let numberToString = String(100);
+
+// Convert string to boolean
+let stringToBoolean = Boolean("true");
